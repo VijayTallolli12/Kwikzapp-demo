c44cfdf29f258a5b03f0bca5bef25e416c94e2ad
Vijay Tallolli <vijaytallolli@gmail.com>
2025-09-06 01:08:13 +0530

    Deploy production build for Github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/Kwikzapp-demo/',
+  base: process.env.VITE_BASE,
   plugins: [
     react(),
     tailwindcss()
